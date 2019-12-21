550f79d2b2aa16e837d781f45a5e7ccf4a23fe36
hieudepchai <hieunguyenvn98@gmail.com>
2019-12-21 23:27:43 +0700

    update

--- graph_process.scala
+++ graph_process.scala
@@ -1,20 +1,39 @@
-./bin/spark-shell --conf spark.neo4j.bolt.password=password --packages neo4j-contrib:neo4j-spark-connector:2.2.1-M5
+// ./bin/spark-shell --conf spark.neo4j.bolt.password=password --packages neo4j-contrib:neo4j-spark-connector:2.2.1-M5
 
 https://dzone.com/articles/neo4j-with-scala-awesome-experience-with-spark
 
+
+// import packages
  import org.neo4j.spark._
  import org.apache.spark.graphx._
  import org.apache.spark.graphx.lib._
 
+ // declare neo variable
  val neo = Neo4j(sc)
 
+ // cypher query
  val graphQuery = """MATCH (n)-[r]->(m) RETURN id(n) as origin,id(m) as destination, r.distance as distance"""
 
+ // load Neo4j graph to GraphX variable
  val graph: Graph[Long, Long] =neo.rels(graphQuery).partitions(1).batch(100).loadGraph
 
-//  
-val rowRDD = neo.cypher("MATCH (n:org) RETURN n.name as name limit 10").loadRowRdd
-// 
-val df = neo.cypher("MATCH (n)-[r]->(m) RETURN n.name as origin, m.name as destination, r.distance as distance").loadDataFrame
+// Number of vertices
+ graph.vertices.count
+// Number of edges
+ graph.edges.count
+// Run PageRank algorithm
+ val g = PageRank.run(graph, 5)
+ //Save "rank" to Neo4j Graph
+ Neo4jGraph.saveGraph(sc, g, "rank")
 
-val rdd = neo.cypher("MATCH (n)-[r]->(m) RETURN n.name as origin, m.name as destination, r.distance as distance").loadRdd
+
+
+
+
+
+//  
+// val rowRDD = neo.cypher("MATCH (n:org) RETURN n.name as name limit 10").loadRowRdd
+// // 
+// val df = neo.cypher("MATCH (n)-[r]->(m) RETURN n.name as origin, m.name as destination, r.distance as distance").loadDataFrame
+
+// val rdd = neo.cypher("MATCH (n)-[r]->(m) RETURN n.name as origin, m.name as destination, r.distance as distance").loadRdd
--- load_data.py
+++ load_data.py
@@ -1,14 +1,21 @@
+# import pandas library to read csv into DataFrame
 import pandas as pd
+# import py2neo library to connect and load data to Neo4j Graph
 from py2neo import Graph, Node, Relationship
+#load data from csv file to dataframe airportDF
 airportDF = pd.read_csv("rita2014jan.csv")
-
-graph = Graph("bolt://localhost:7687",auth=("neo4j", "password"))
+#declare a graph variable which connects to Neo4j Graph
+graph = Graph("bolt://localhost:7687", auth=("neo4j", "password"))
+#clear all graph data ( if existed)
 graph.delete_all()
-
-print(airportDF)
+#iterate rows in airportDF
 for index, row in airportDF.iterrows():
-    node_a = Node("org", name=row['origin'])
-    node_b = Node("dest", name=row['dest'])
+    #create origin node which is the column 'origin' in airportDF and its label is "org"
+    node_a = Node("airport", name=row['origin'])
+    #create destination node which is the column 'dest' in airportDF and its label is "dest"
+    node_b = Node("airport", name=row['dest'])
+    #create a relationship between those node, label it as "dist" and add a property "distance" which is the column dist in airportDF 
     rel = Relationship(node_a,"dist", node_b, distance=row['dist'])
-    graph.merge(rel,"dist", "name")
+    #merge the relationship into the graph variable
+    graph.merge(rel, "airport", "name")
     
